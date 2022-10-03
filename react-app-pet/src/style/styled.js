2c7020b2e1a9cc2a670ff8795971ec41423fb261
EmersonGald1n0 <rm93899@fiap.com.br>
2022-10-02 22:59:26 -0300

    add arquivo styled.js p estilizar conteúdo da page

---
+++ react-app-pet/src/style/styled.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export const DivLista = styled.div`
+  width: 100%;
+  padding: 20px;
+  min-height: 95vh;
+  background-color: #99FCD6;
+  display: flex;
+  flex-wrap: wrap;
+  font-family: 'Trebuchet MS';
+`
